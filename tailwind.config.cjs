caec48dadaf57b72ac7c555ddb9633f917fc1b82
Arie <kelivdc@yahoo.com>
2023-03-21 22:58:46 +0700

    Category

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+// https://colorhunt.co/palette/3a98b9fff1dce8d5c4eeeeee
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
