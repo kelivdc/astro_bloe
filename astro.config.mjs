19a52c43173f3bd8773a518f1f3c3b9e82bc488f
Arie <kelivdc@yahoo.com>
2023-03-22 21:31:35 +0700

    test compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,17 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 
-import frontendistahtmlMinify from "@frontendista/astro-html-minify";
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  integrations: [tailwind(), frontendistahtmlMinify()],
-  adapter: vercel()
+  output: "server",
+  integrations: [tailwind(), 
+    compress({
+      html: true,
+      path: "./build",
+    })
+  ],
+  adapter: vercel(),
 });
