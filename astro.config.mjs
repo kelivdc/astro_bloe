6b86ceb896629e249e75c4f9f2555e8c2c499307
Arie <kelivdc@yahoo.com>
2023-03-22 18:55:43 +0700

    add minify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import frontendistahtmlMinify from "@frontendista/astro-html-minify";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  integrations: [tailwind()],
+  integrations: [tailwind(), frontendistahtmlMinify()],
   adapter: vercel()
 });
