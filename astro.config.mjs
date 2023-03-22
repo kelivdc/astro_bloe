d73ed7831486115bf4ca3717434f5a68c7c50d61
Arie <kelivdc@yahoo.com>
2023-03-22 22:39:30 +0700

    Test lagui

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,10 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 import compress from "astro-compress";
-import htmlMinify from "@frontendista/astro-html-minify";
-
-import htmlMinifier from "astro-html-minifier";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  outDir: "./build",
-  integrations: [tailwind(), compress(), htmlMinify(), htmlMinifier()],
+  integrations: [tailwind(), compress()],
   adapter: vercel()
 });
