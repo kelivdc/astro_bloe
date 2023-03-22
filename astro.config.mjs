95d9c39893fc8c31981e08a86648c5376127051c
Arie <kelivdc@yahoo.com>
2023-03-22 22:32:13 +0700

    Test minifier

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,14 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 import compress from "astro-compress";
-
 import htmlMinify from "@frontendista/astro-html-minify";
+
+import htmlMinifier from "astro-html-minifier";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   outDir: "./build",
-  integrations: [tailwind(), compress(), htmlMinify()],
+  integrations: [tailwind(), compress(), htmlMinify(), htmlMinifier()],
   adapter: vercel()
 });
