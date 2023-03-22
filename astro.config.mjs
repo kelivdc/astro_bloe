e817f43b45501a2a4e85e2264ef8eac36f13cc5c
Arie <kelivdc@yahoo.com>
2023-03-22 21:44:49 +0700

    outdir Build

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import compress from "astro-compress";
 // https://astro.build/config
 export default defineConfig({
   output: "server",
+  outDir: "./build",
   integrations: [tailwind(), 
     compress({
       html: true,
