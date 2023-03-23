381d3fa3b7352ad972c17152f98ef806c95a6ad3
Arie <kelivdc@yahoo.com>
2023-03-23 18:56:35 +0700

    Test image

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,11 @@ import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 import compress from "astro-compress";
 
+import image from "@astrojs/image";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  integrations: [
-    tailwind(),
-    compress(),
-  ],
-  adapter: vercel(),
+  integrations: [tailwind(), compress(), image()],
+  adapter: vercel()
 });
